e0e9b92a7dc8df5e71491782f2ed111763d267ff
Egbert Eich <eich@suse.de>
2004-02-26 09:23:27 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- src/leo.h
+++ src/leo.h
@@ -20,7 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/sunleo/leo.h,v 1.4 2001/05/04 19:05:46 dawes Exp $ */
+/* $XFree86$ */
 
 #ifndef LEO_H
 #define LEO_H
